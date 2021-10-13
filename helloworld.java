98f59875b29752ec5c93409bbb1b4941a742463f
Kokkales <kokkaleskonstantinos@gmail.com>
2021-10-13 17:56:16 +0300

    Update helloworld.java
    εγω το εκανα αυτο

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,24 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}  
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author kokka
+ */
+public class helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello World!!");
+    }
+      
+}
