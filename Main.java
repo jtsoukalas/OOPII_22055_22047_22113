a9073036c52ce669123df9b9f669299c5d0cdb54
Kokkales <kokkaleskonstantinos@gmail.com>
2021-10-13 18:33:38 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.orestis_package;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //test
+        System.out.println("Hello World");
+        System.out.println("Change!");
+        //test2
+        System.out.println("BLA BLA");
+        //test3
+        //test4
+    }
+}
