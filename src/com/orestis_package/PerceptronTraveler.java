34f9c7ae25ebd345aca9ae7f951e2ed82573c76a
Jason Tsoukalas <jasontsoukalas@icloud.com>
2021-10-19 20:38:18 +0300

    Adding some comments

--- src/com/orestis_package/PerceptronTraveler.java
+++ src/com/orestis_package/PerceptronTraveler.java
@@ -3,7 +3,7 @@ package com.orestis_package;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public  interface PerceptronTraveler {
+public interface PerceptronTraveler {
 
     public ArrayList recommend(Array data);
 
--- src/com/orestis_package/PerceptronYoungTraveler.java
+++ src/com/orestis_package/PerceptronYoungTraveler.java
@@ -7,18 +7,18 @@ import java.util.Locale;
 public class PerceptronYoungTraveler implements PerceptronTraveler {
 
 
-    public ArrayList recommend(City data) {
+    public ArrayList recommend (City data) {
         //add code
-        return null;
+        return null;        //temporarily return
     }
 
-    public ArrayList recommend(City data, boolean uppercase) {
+    public ArrayList recommend (City data, boolean uppercase) {
 
         ArrayList recommendation = recommend(data);
 
         if (uppercase) {
             for (int i = 0; i < recommendation.size(); i++) {
-                recommendation.get(i).toString().toUpperCase(); //Needs testing
+                recommendation.get(i).toString().toUpperCase();     //Needs testing
             }
         }
         return recommendation;
