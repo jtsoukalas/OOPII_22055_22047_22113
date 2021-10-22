df16347142e5a4c3486c0eb9c1e8497649f254b7
OrestisKr <it22055@hua.gr>
2021-10-22 19:18:10 +0300

    Recommend and uppercase method

--- src/firstDeliverable/PerceptronElderTraveler.java
+++ src/firstDeliverable/PerceptronElderTraveler.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 public class PerceptronElderTraveler extends PerceptronTraveler {
 
 
-    private final float weightsBias[] = new float[]{3F, 1F, 1.5F, 3F, 0F, 0.5F, 2.5F, 3F, 3F, 3F};              //||1.cafe,restaurant 2.sea, 3.museums, 4.wellness center, 5.stadium,
-                                                                                                                  // 6.bar/club, 7.parks,playgrounds, 8.temperature, 9.weather(how cloudy), 10.distance||
-                                                                                                                  // Convention of weightBias: min = 0 max = 3
 
+    public PerceptronElderTraveler() {
+        super(new float[]{3F, 1F, 1.5F, 3F, 0F, 0.5F, 2.5F, 3F, 3F, 3F});
+    }
 }
--- src/firstDeliverable/PerceptronTraveler.java
+++ src/firstDeliverable/PerceptronTraveler.java
@@ -4,43 +4,54 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public abstract class PerceptronTraveler {
+
+    // 1.cafe,restaurant 2.sea, 3.museums, 4.wellness center, 5.stadium,
+    // 6.bar/club, 7.parks,playgrounds, 8.temperature, 9.weather(how cloudy), 10.distance
+    // Convention of weightBias: min = 0 max = 3
+
     private float weightsBias[] = new float[]{0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F};
 
-    public ArrayList recommend(City data) {
-        //add code
-        return null;            //temporarily return
+    public PerceptronTraveler(float[] weightsBias) {
+        this.weightsBias = weightsBias;
     }
 
-    public ArrayList recommend(City data, boolean uppercase) {
-        ArrayList recommendation = recommend(data);
+    public float[] getWeightsBias() {
+        return weightsBias;
+    }
 
-        if (uppercase) {
-            for (int i = 0; i < recommendation.size(); i++) {
-                recommendation.get(i).toString().toUpperCase(); //Needs testing
-            }
+    public String recommend(Boolean[] compatibleCities, City[] citiesLibrary) {
+
+        String recommend = "";
+        for (int cityCounter = 0; cityCounter < compatibleCities.length; cityCounter++) {
+            recommend += compatibleCities[cityCounter] ? citiesLibrary[cityCounter].getName() : ", ";     //!!!!
         }
-        return recommendation;
+        return recommend;
+    }
+
+    public String recommend(Boolean[] compatibleCities, City[] citiesLibrary, boolean uppercase) {
+
+        String recommendation = recommend(compatibleCities, citiesLibrary);
+
+        return  uppercase? recommendation.toUpperCase():recommendation;
     }
 
     public boolean[] retrieveCompatibleCities(City citiesLibrary[]) {
         boolean approvedCities[] = new boolean[citiesLibrary.length];
 
         for (int cityCounter = 0; cityCounter < approvedCities.length; cityCounter++) {
-            float sum=0;
+            float sum = 0;
             for (int featureCounter = 0; featureCounter < 10; featureCounter++) {
-                sum+=citiesLibrary[cityCounter].getFeatures()[featureCounter]*weightsBias[featureCounter];
+                sum += citiesLibrary[cityCounter].getFeatures()[featureCounter] * weightsBias[featureCounter];
             }
             //bias
-            approvedCities[cityCounter]=sum>0?true:false;
+            approvedCities[cityCounter] = sum > 0 ? true : false;
         }
         //!!!   BIAS   !!!!!
 
         return approvedCities;
     }
 
-    public float[] getWeightsBias() {
-        return weightsBias;
-    }
+
 }
 
 
