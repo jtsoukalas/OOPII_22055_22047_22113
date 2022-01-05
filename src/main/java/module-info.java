a15b3c56c5b0a9585448dc5d8edbe55910d12e9e
OrestisKr <it22055@hua.gr>
2022-01-05 22:54:50 +0200

    modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -16,7 +16,8 @@ module gr.hua.oopii.travelAgency {
     requires commons.lang;
     requires org.testng;
     requires junit;
-    requires java.sql;  //for json deserialization
+    requires java.sql;
+    requires jdk.internal.le;  //for json deserialization
 
 
     opens gr.hua.oopii.travelAgency.GUI to javafx.fxml;
